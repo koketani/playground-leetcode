08efc7cfbb0dae271b0fb89bb88e59e5f396e492
koketani <ykoketani@gmail.com>
2020-02-11 12:06:57 +0900

    correct 1. Two Sum

--- 1_two_sum/two_sum.py
+++ 1_two_sum/two_sum.py
@@ -3,9 +3,12 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i, first in enumerate(nums):
-            for j, second in enumerate(nums):
-                if i >= j:
-                    continue
-                if first + second == target:
-                    return [i, j]
+        rev = {}
+        for k, v in enumerate(nums):
+            rev[v] = k
+        for k, v in enumerate(nums):
+            kk = rev.get(target-v)
+            if not kk or k == kk:
+                continue
+            return [k, kk]
+        return [-1, -1]
